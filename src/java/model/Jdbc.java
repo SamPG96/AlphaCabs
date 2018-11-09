49b3951343833653bda46822b576814359673a50
Conor Newcombe <newcombe74@live.co.uk>
2018-11-09 13:22:59 +0000

    Implemented retrieve funtionality

--- src/java/model/Jdbc.java
+++ src/java/model/Jdbc.java
@@ -14,155 +14,168 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
  * @author me-aydin
  */
 public class Jdbc {
-    
+
     Connection connection = null;
     Statement statement = null;
     ResultSet rs = null;
     //String query = null;
+
+    public static void main(String[] args){
+        Connection conn;
+        Jdbc jdbc = new Jdbc();
+        
+        String dbpath = "jdbc:derby://localhost:1527/alphacabsdb";
+        String dbuser = "ateam";
+        String dbpass = "ateam";
+        
+        try {
+            //Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            // TODO: make user and password variable
+            conn = DriverManager.getConnection(dbpath, dbuser, dbpass);
+            
+            jdbc.connect(conn);
+            ArrayList test = jdbc.retrieve("Users","Username","jsmith");
+            System.out.println(test.get(0).toString());
+        }
+        catch(ClassNotFoundException | SQLException e){
+            System.err.println(e);
+        }
+        
+    }
     
-    
-    public Jdbc(String query){
+    public Jdbc(String query) {
         //this.query = query;
     }
 
     public Jdbc() {
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
-    public void connect(Connection con){
-       connection = con;
+
+    public void connect(Connection con) {
+        connection = con;
     }
-    
+
     private ArrayList rsToList() throws SQLException {
         ArrayList aList = new ArrayList();
 
         int cols = rs.getMetaData().getColumnCount();
-        while (rs.next()) { 
-          String[] s = new String[cols];
-          for (int i = 1; i <= cols; i++) {
-            s[i-1] = rs.getString(i);
-          } 
-          aList.add(s);
-        } // while    
-        return aList;
-    } //rsToList
- 
-    private String makeTable(ArrayList list) {
-        StringBuilder b = new StringBuilder();
-        String[] row;
-        b.append("<table border=\"3\">");
-        for (Object s : list) {
-          b.append("<tr>");
-          row = (String[]) s;
-            for (String row1 : row) {
-                b.append("<td>");
-                b.append(row1);
-                b.append("</td>");
+        while (rs.next()) {
+            String[] s = new String[cols];
+            for (int i = 1; i <= cols; i++) {
+                s[i - 1] = rs.getString(i);
             }
-          b.append("</tr>\n");
-        } // for
-        b.append("</table>");
-        return b.toString();
-    }//makeHtmlTable
-    
-    private void select(String query){
-        //Statement statement = null;
+            aList.add(s);
+        }
+        return aList;
+    }
+
+    private ArrayList<Map<String, String>> rsToMaps() throws SQLException {
+        ArrayList<Map<String, String>> ret = new ArrayList<>();
+        Map<String, String> rowMap;
         
+        int nCols = rs.getMetaData().getColumnCount();
+        String[] colNames = new String[nCols];
+        for (int c = 0; c < nCols; c++) {
+            colNames[c] = rs.getMetaData().getColumnName(c);
+        }
+
+        while (rs.next()) {
+            rowMap = new HashMap<>();
+            for (int c = 1; c <= nCols; c++) {
+                rowMap.put(colNames[c], rs.getString(c));
+            }
+            ret.add(rowMap);
+        }
+        return ret;
+    }
+
+    private void select(String tableName, String colName, String value) {
+        String query = "SELECT * FROM " + tableName + " WHERE " + colName + " EQUALS " + value;
+
         try {
             statement = connection.createStatement();
             rs = statement.executeQuery(query);
-            //statement.close();
-        }
-        catch(SQLException e) {
-            System.out.println("way way"+e);
-            //results = e.toString();
+        } catch (SQLException e) {
+            System.out.println(e);
         }
     }
-    public String retrieve(String query) throws SQLException {
-        String results="";
-        select(query);
-//        try {
-//            
-//            if (rs==null)
-//                System.out.println("rs is null");
-//            else
-//                results = makeTable(rsToList());
-//        } catch (SQLException ex) {
-//            Logger.getLogger(Jdbc.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-        return makeTable(rsToList());//results;
-    }
-    
-    public boolean exists(String user) {
-        boolean bool = false;
-        try  {
-            select("select username from users where username='"+user+"'");
-            if(rs.next()) {
-                System.out.println("TRUE");         
-                bool = true;
+
+    public ArrayList<Map<String, String>> retrieve(String tableName, String colName, String value) throws SQLException {
+        select(tableName, colName, value);
+
+        try {
+            if (rs == null) {
+                System.out.println("rs is null");
+            } else {
+                return rsToMaps();
             }
         } catch (SQLException ex) {
             Logger.getLogger(Jdbc.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return bool;
+        return null;//results;
     }
-    public void insert(String[] str){
+
+    public void insert(String[] str) {
         PreparedStatement ps = null;
         try {
-            ps = connection.prepareStatement("INSERT INTO Users VALUES (?,?)",PreparedStatement.RETURN_GENERATED_KEYS);
-            ps.setString(1, str[0].trim()); 
+            ps = connection.prepareStatement("INSERT INTO Users VALUES (?,?)", PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setString(1, str[0].trim());
             ps.setString(2, str[1]);
             ps.executeUpdate();
-        
+
             ps.close();
             System.out.println("1 row added.");
         } catch (SQLException ex) {
             Logger.getLogger(Jdbc.class.getName()).log(Level.SEVERE, null, ex);
         }
-         
+
     }
+
     public void update(String[] str) {
         PreparedStatement ps = null;
         try {
-            ps = connection.prepareStatement("Update Users Set password=? where username=?",PreparedStatement.RETURN_GENERATED_KEYS);
-            ps.setString(1, str[1].trim()); 
+            ps = connection.prepareStatement("Update Users Set password=? where username=?", PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setString(1, str[1].trim());
             ps.setString(2, str[0].trim());
             ps.executeUpdate();
-        
+
             ps.close();
             System.out.println("1 rows updated.");
         } catch (SQLException ex) {
             Logger.getLogger(Jdbc.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    public void delete(String user){
-       
-      String query = "DELETE FROM Users " +
-                   "WHERE username = '"+user.trim()+"'";
-      
+
+    public void delete(String user) {
+
+        String query = "DELETE FROM Users "
+                + "WHERE username = '" + user.trim() + "'";
+
         try {
             statement = connection.createStatement();
             statement.executeUpdate(query);
-        }
-        catch(SQLException e) {
-            System.out.println("way way"+e);
+        } catch (SQLException e) {
+            System.out.println("way way" + e);
             //results = e.toString();
         }
     }
-    public void closeAll(){
+
+    public void closeAll() {
         try {
             rs.close();
-            statement.close(); 		
+            statement.close();
             //connection.close();                                         
-        }
-        catch(SQLException e) {
+        } catch (SQLException e) {
             System.out.println(e);
         }
-    }       
+    }
 }
