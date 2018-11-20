87a2189a7ff1d6ad8fd400cc2b5ebf118825b0b6
Conor Newcombe <Newcombe74@live.co.uk>
2018-11-20 13:13:35 +0000

    Added BookingManagement

---
+++ src/java/model/BookingManagement.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Conor
+ */
+public class BookingManagement {
+    
+    
+    
+    public double calcDistanceKM(String source, String dest){
+        return 0.0;
+    }
+}
